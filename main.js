30bcbe876e6778441bab0a6fef488e40d7a7a066
Jauhar Arifin <jauhararifin10@gmail.com>
2023-09-14 06:45:43 +0700

    fix click tap and fire event

--- main.js
+++ main.js
@@ -46,16 +46,20 @@ window.onload = function() {
         canvas.height = window.innerHeight;
         results.instance.exports.on_resize(canvas.width, canvas.height);
       })
+      const flag = false;
       window.addEventListener('click', function() {
+        if (flag) return; flag = true; setTimeout(() => { flag = false }, 5);
         results.instance.exports.on_click();
         return false;
       })
       window.addEventListener('touchstart', function() {
+        if (flag) return; flag = true; setTimeout(() => { flag = false }, 5);
         results.instance.exports.on_click();
         return false;
       })
       window.addEventListener('keypress', function(ev) {
         if (ev.key !== " ") return;
+        if (flag) return; flag = true; setTimeout(() => { flag = false }, 5);
         results.instance.exports.on_click();
         return false;
       })
