b247136782828814d3e97de9991129ab3869dc52
Jauhar Arifin <jauhararifin10@gmail.com>
2023-09-29 11:09:24 +0700

    fix optimization logic in build script

--- build.sh
+++ build.sh
@@ -18,13 +18,19 @@ embed pipe_bmp pipe.bmp >> embed.mg
 embed digits_bmp digits.bmp >> embed.mg
 embed favicon_ico favicon.ico >> embed.mg
 
+echo Compiling
 magelang compile main -o main.wasm
 
-./wasm-opt --enable-reference-types --enable-multivalue -O4 main.wasm -o main.opt.wasm
+echo Optimizing
+./wasm-opt --enable-reference-types --enable-multivalue -O4 main.wasm -o main.opt.wasm > /dev/null 2>&1
+if [ ! -f main.opt.wasm ]; then
+  echo "wasm-opt program cannot be executed in your machine, will use unoptimized webassembly"
+  cp main.wasm main.opt.wasm
+fi
 
 mkdir -p build/
 cp index.html build/index.html
 cp main.js build/main.js
-cp main.wasm build/main.wasm
-cp main.opt.wasm build/main.opt.wasm
+mv main.wasm build/main.wasm
+mv main.opt.wasm build/main.opt.wasm
 cp favicon.ico build/favicon.ico
